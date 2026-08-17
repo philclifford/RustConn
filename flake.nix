0706dafd942019915d67ee30c8db9820406b7586
Anton Isaiev <totoshko88@gmail.com>
2026-08-17 17:23:06 +0300

    chore(release): version propagation to all packaging/docs files

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "rustconn";
-          version = "0.20.2";
+          version = "0.20.3";
 
           src = self;
 
